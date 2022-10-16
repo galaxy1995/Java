cbc9dc961e423ddbc2e1cfd99fb961c2e51518f5
galaxy1995 <115927128+galaxy1995@users.noreply.github.com>
2022-10-16 18:06:49 +0530

    Add files via upload

---
+++ ScreenShotWithCurrentDate.java
@@ -0,0 +1,16 @@
+package Selenium;
+
+import java.io.IOException;
+import java.sql.Date;
+
+public class ScreenShotWithCurrentDate {
+	/*public static void SS() throws IOException{
+		Date currentdate = new Date();
+		String screenshotfilename = currentdate.toString().replace(" ","-").replace(":", "-");
+		System.out.println(screenshotfilename);
+		System.setProperty("webdriver.chrome.driver","ChromeDriver.exe");
+		//WebDriver driver = new 	ChromeDriver ();
+	} */
+	public static void main (String [] args) {	
+	}
+}
